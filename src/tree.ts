653de945b25219652a9caa259e307323f29ee508
Marijn Haverbeke <marijn@haverbeke.nl>
2021-02-18 11:32:39 +0100

    Add missing doc comments in TreeBuffer

--- src/tree.ts
+++ src/tree.ts
@@ -402,10 +402,11 @@ export class TreeBuffer {
   constructor(
     /// @internal
     readonly buffer: Uint16Array,
-    // The total length of the group of nodes in the buffer.
+    /// The total length of the group of nodes in the buffer.
     readonly length: number,
     /// @internal
     readonly set: NodeSet,
+    /// An optional repeat node type associated with the buffer.
     readonly type = NodeType.none
   ) {}
 
